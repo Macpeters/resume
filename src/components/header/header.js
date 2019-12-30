63317fa33dde5c713102fbbe11446cb843bda139
Mac Peters <macpetersdev@gmail.com>
2019-12-30 06:31:04 -0500

    full name

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,7 @@ export default class Header extends React.Component {
   render() {
     return(
       <div className="header">
-        <h1>Mac Peters</h1>
+        <h1>Maryanne (Mac) Peters</h1>
         <h2>www.macpeters.com</h2>
         <hr />
       </div>
