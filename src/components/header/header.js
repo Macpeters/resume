0f2a12965ff2a05c82a997ff120379d4a089ce75
Mac Peters <macpetersdev@gmail.com>
2020-01-26 09:58:39 -0500

    try making the header with flex-box with mixed results

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './header.css';
+import logoImage from '../../images/logo.png';
 
 export default class Header extends React.Component {
   render() {
     return(
       <div className="header">
-        <h1>Maryanne (Mac) Peters</h1>
-        <h2>www.macpeters.com</h2>
-        <hr />
+        <div class="logo">
+          <img className="header-logo" src={logoImage} alt="logo" />
+        </div>
+        <div className="header-text">
+          <h1>Maryanne (Mac) Peters</h1>
+          <h2>www.macpeters.com</h2>
+        </div>
+        <div class="header-description">
+          <h3>Web Development</h3>
+          <h3>Games</h3>
+        </div>
       </div>
     )
   }
